6bfc47ba7724f4c68b959e00494d7351f8bb8991
Kaue Jorge <thekauej@gmail.com>
2022-05-06 11:19:03 -0300

    Configurando Movimentações

--- src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
@@ -3,6 +3,7 @@ package com.dio.santander.bankline.api.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,8 +33,15 @@ public class MovimentacaoController {
 		return repository.findAll();
 	}
 	
+	@GetMapping("/{idConta}")
+	public List<Movimentacao> findAll(@PathVariable("idConta") Integer idConta){
+		return repository.findByIdConta(idConta);
+	}
+	
 	@PostMapping
 	public void save(@RequestBody NovaMovimentacao movimentacao) {
 		service.save(movimentacao);
 	}
+	
+	
 }
